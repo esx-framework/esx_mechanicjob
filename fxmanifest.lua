b17dddae45520295b6b955cc96d7fcaa53efc821
Kasey Fitton <kaseyfitton@hotmail.co.uk>
2024-02-11 16:03:42 +0000

    tweak(fxmanifest): update description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 
 game 'gta5'
 
-description 'ESX Mechanic Job'
+description 'Allows players to RP as a mechanic (repair and modify vehicles)'
 lua54 'yes'
 version '1.0'
 legacyversion '1.9.1'
